56bd6331e39a42c5e151e4abc8d18b73e6c4e61f
pedrohenriquepereirasampaio8 <pedrohenriquepereirasampaio8@gmail.com>
2026-03-28 21:16:28 -0300

    Add files via upload

---
+++ Gerador de Senhas.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def gerar_senha(tamanho=8):
+    caracteres = string.ascii_letters + string.digits
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+print("Senha gerada:", gerar_senha(10))
